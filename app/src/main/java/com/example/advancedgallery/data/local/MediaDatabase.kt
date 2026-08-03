6f2c2340680050729e13673877d2c94a541607f0
aktarjabed <33180459+aktarjabed@users.noreply.github.com>
2026-08-03 07:59:41 +0530

    Revert "Implement Advanced Gallery App ok"

--- app/src/main/java/com/example/advancedgallery/data/local/MediaDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.advancedgallery.data.local
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [MediaEntity::class], version = 1, exportSchema = false)
-abstract class MediaDatabase : RoomDatabase() {
-    abstract fun mediaDao(): MediaDao
-}
